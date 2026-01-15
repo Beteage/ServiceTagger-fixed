4856264ff89651d5d0af8f7475d34e05be369afd
Luke Beteag <lukebeteag@Lukes-MacBook-Air.local>
2026-01-14 20:12:53 -0800

    Remove leftover legacy API files

--- api/[...all].ts
+++
@@ -1,7 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-import app from '../server/src/app';
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-    // Pass the request to Express app
-    return app(req, res);
-}
